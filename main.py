1cc1d306ca584d48e1dadc1a822e7d15969e93e4
asasxa <163177231+asasxa@users.noreply.github.com>
2024-10-29 18:31:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,44 @@ if os.path.exists(dotenv_path):
 VK_TOKEN = os.getenv('VK_TOKEN')
 YA_TOKEN = os.getenv('YA_TOKEN')
 
-class VKConnector:
+
+class HttpException(Exception):
+    """Класс исключения, выбрасываем, когда API возвращает ошибку"""
+
+    def __init__(self, status, message=''):
+        self.status = status
+        self.message = message
+
+    def __str__(self):
+        return f'http error: {self.status}\n{self.message}'
+
+
+class ApiBasic:
+    host = ''
+
+    def _send_request(self, http_method, uri_path, params=None, json=None, headers=None, response_type=None):
+        response = requests.request(http_method, f'{self.host}/{uri_path}', params=params, json=json, headers=headers)
+
+        if response.status_code >= 400:
+            raise HttpException(response.status_code, response.text)
+
+        if response_type == 'json':
+            return response.json()
+        return response.text
+
+class VKConnector(ApiBasic):
+    host = 'https://api.vk.com/method/'
+
     def __init__(self, access_token, version=5.199):
         self.access_token = access_token
         self.version = version
-        self.base_url = 'https://api.vk.com/method/'
         self.params = {
             'access_token': self.access_token,
             'v': self.version
         }
 
     def photos_info(self, owner_id, count=5, extended=1, photo_sizes=1):
-        url = f'{self.base_url}photos.get'
+        url = 'photos.get'
         params = {
             **self.params,
             'owner_id': owner_id,
@@ -33,28 +59,24 @@ class VKConnector:
             'extended': extended,
             'photo_sizes': photo_sizes
         }
-        response = requests.get(url, params=params)
-        return response.json()
+        return self._send_request('GET', url, params=params, response_type='json')
 
-class YAConnector:
+class YAConnector(ApiBasic):
+    host = 'https://cloud-api.yandex.net/v1/disk'
+
     def __init__(self, token):
         self.headers = {'Authorization': f'OAuth {token}'}
 
     def create_folder(self, folder_name):
-        url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
-        response = requests.put(url=url_create_folder, 
-                                headers=self.headers, 
-                                params={'path': folder_name})
-        return response.status_code
-    
+        url = 'resources'
+        return self._send_request('PUT', url, params={'path': folder_name}, headers=self.headers)
+
     def upload_image(self, image_name, photo_url):
-        url_upload_image = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        response = requests.post(url=url_upload_image,
-                                 headers=self.headers,
-                                 params={'path': f'/{FOLDER_NAME}/{image_name}',
-                                         'url': photo_url,
-                                        })
-        return response.status_code                        
+        url = 'resources/upload'
+        return self._send_request('POST', url, params={
+            'path': f'/{FOLDER_NAME}/{image_name}',
+            'url': photo_url,
+        }, headers=self.headers)
 
 FOLDER_NAME = 'VK_photo_loaded'
 vk_connector = VKConnector(VK_TOKEN)
@@ -72,7 +94,7 @@ total_uploads = total_photos
 total_file_writes = 1  
 total_operations = total_photos + total_uploads + total_file_writes
 
-with tqdm(total=100, desc="Прогресс", unit="операций") as pbar:
+with tqdm(total=100, desc="Прогресс", unit="%") as pbar:
     for item in photos_info['response']['items']:
         likes_count = item['likes']['count']
         date = item['date']
@@ -85,16 +107,8 @@ with tqdm(total=100, desc="Прогресс", unit="операций") as pbar:
         likes_check_dict[likes_count] = 1
 
         image_sizes = item['sizes']
-        best_image = None
-        max_height =  0
-        max_width =  0
+        best_image = max(image_sizes, key=lambda x: (x['height'], x['width']), default=None)
 
-        for image in image_sizes:
-            if (image['height'] > max_height) and (image['width'] > max_width):
-                max_height = image['height']
-                max_width = image['width']
-                best_image = image
-        
         if best_image:
             image_name_dict[image_name] = best_image['url']
         else:
@@ -102,7 +116,7 @@ with tqdm(total=100, desc="Прогресс", unit="операций") as pbar:
 
         photo_info_json.append({
             "file_name": f"{image_name}",
-            "size": f"{best_image['type']}"
+            "size": f"{best_image['type'] if best_image else 'unknown'}"
         })
 
         pbar.update(1 * 100 / total_operations)  
@@ -111,7 +125,7 @@ with tqdm(total=100, desc="Прогресс", unit="операций") as pbar:
         ya_connector.upload_image(image_name, photo_url)
         pbar.update(1 * 100 / total_operations) 
 
-    with open('photo_info.json', 'w') as file:
+    with open('photo_info.json', 'w', encoding='utf-8') as file:
         json.dump(photo_info_json, file, ensure_ascii=False, indent=0)
     
     pbar.update(1 * 100 / total_operations)
